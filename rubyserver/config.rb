a9ca1483561559f56060d5b100f377996e0a69cf
devinclary <devinclary@me.com>
2016-02-27 16:10:12 -0800

    Implements HttpConfig class: -Uses ConfigFile super class to handle basic parsing -Provides further parsing that is specific to httpd.conf files -Provides accessor methods for the supported configuration options

--- rubyserver/config.rb
+++ rubyserver/config.rb
@@ -1,6 +1,5 @@
 #Parent class to provide common config file parsing
 class ConfigFile
-
   attr_reader :lines
 
   def initialize(str)
@@ -65,13 +64,106 @@ class MimeTypes < ConfigFile
 end
 
 class HttpConfig < ConfigFile
+  attr_reader :config
 
+  def initialize(str)
+    super
+    self.process_lines()
+  end
 
+  def process_lines()
+    @config=Hash.new()
+    line_hash=load() #retrieves hash of the multi-line string
+    line_hash.each do |config_option, config_values|
+      @config[config_option]=Array.new()
+      config_values.split(' ').each_with_index do |config_value, iteration|
+        @config[config_option][iteration]=config_value
+      end
+    end
+  end
+
+  #TODO: implement exception throwing when attributes aren't found in accessors
+  def server_root()
+    if(!@config.has_key?('ServerRoot'))
+      return ""
+    end
+    return @config['ServerRoot'][0]
+  end
+
+  def document_root()
+    if(!@config.has_key?('DocumentRoot'))
+      return ""
+    end
+    return @config['DocumentRoot'][0]
+  end
+
+  def listen()
+    if(!@config.has_key?('Listen'))
+      return ""
+    end
+    return @config['Listen'][0]
+  end
+
+  def log_file()
+    if(!@config.has_key?('LogFile'))
+      return ""
+    end
+    return @config['LogFile'][0]
+  end
+
+  def alias_sym()
+    if(!@config.has_key?('Alias'))
+      return ""
+    end
+    return @config['Alias'][0]
+  end
+
+  def alias_abs()
+    if(!@config.has_key?('Alias'))
+      return ""
+    end
+    return @config['Alias'][1]
+  end
+
+  def script_alias_sym()
+    if(!@config.has_key?('ScriptAlias'))
+      return ""
+    end
+    return @config['ScriptAlias'][0]
+  end
+
+  def script_alias_abs()
+    if(!@config.has_key?('ScriptAlias'))
+      return ""
+    end
+    return @config['ScriptAlias'][1]
+  end
+
+  def access_file_name()
+    if(!@config.has_key?('AccessFileName'))
+      return ""
+    end
+    return @config['AccessFileName'][0]
+  end
+
+  def directory_indexes()
+    if(!@config.has_key?('Listen'))
+      return ""
+    end
+    return @config['DirectoryIndex']
+  end
 end
 
-## Here's some code to illustrate how this works!
-configFile=MimeTypes.new(File.open("config/mime.types", "r").read()).load
-configFile.mime_types.each do |k,v|
+# Here's some code to illustrate how this works!
+mimes=MimeTypes.new(File.open("config/mime.types", "r").read()).load
+mimes.mime_types.each do |k,v|
   print k, " : ",v, "\n"
 end
-puts configFile.for('h261')
+puts mimes.for('h261')
+
+httpd_conf=HttpConfig.new(File.open("config/httpd.conf", "r").read())
+print "Server Root: ", httpd_conf.server_root(),"\n"
+print "Document Root: ", httpd_conf.document_root(),"\n"
+print "Listen: ", httpd_conf.listen(),"\n"
+print "Log File: ",httpd_conf.log_file(),"\n"
+
