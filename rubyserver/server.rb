9ef3bb6ca22d73b1a58962ba1c4ee8cbaa92312f
lierluis <lierluis1@gmail.com>
2016-02-27 17:55:43 -0800

    changed some identifiers for clarity

--- rubyserver/server.rb
+++ rubyserver/server.rb
@@ -17,18 +17,24 @@ class WebServer
   def start
     @portnumber = @httpd.port
 
-    loop do
-	  puts "\n-----------------------------------------------"
+    loop do  
+      puts "-----------------------------------------------"
       puts "Opening server socket to listen for connections"
       @socket = server.accept # open socket, wait until client connects
       
-	  Thread.new(@socket) do |newsocket| #Thread for every session
-        puts "Received connection\n"
-        Request.new(newsocket).parse
-        newsocket.puts Response.new.to_s
-        
-        newsocket.close # terminate connection
-      end
+      puts "Received connection\n\n"
+      Request.new(@socket).parse # parse client's HTTP request
+      @socket.puts Response.new.to_s # print HTTP response to client
+      
+      @socket.close # terminate connection
+      
+#	  Thread.new(@socket) do |newsocket| # thread for every session
+#        puts "Received connection\n"
+#        Request.new(newsocket).parse
+#        newsocket.puts Response.new.to_s
+#        
+#        newsocket.close # terminate connection
+#      end
     end
   end
   
@@ -40,17 +46,17 @@ end
 
 # receives a stream in constructor, & parses content into members
 class Request
-  attr_reader :verb, :uri, :query, :version, :headers, :body, :session
+  attr_reader :verb, :uri, :query, :version, :headers, :body, :request
   
   def initialize(stream)
-    @session = stream
+    @request = stream
   end
   
   def parse
-    request = @session.gets
-    print "request: ", request
+    request_line = @request.gets
+    print "request line: ", request_line
     
-    fullpath = request.split(" ")
+    fullpath = request_line.split(" ")
     path, query = fullpath[1].split("?")
     
     @body    = "body"
@@ -60,21 +66,33 @@ class Request
     @version = fullpath[2]
     @headers = Hash.new
     
-    while (header = @session.gets) != "\r\n"
+    has_body = false
+    while (header = @request.gets) != "\r\n"
       key, value = header.split(": ")
       @headers.store(key, value)
+      
+      if key == "Content-Length"
+        has_body = true
+      end
     end
     
     @headers.each do |key, value|
       puts "#{key}: #{value}"
     end
-    print "\r\n" # blank line
-    puts "#{@body}"
+    puts "#{[header]}" # blank line
+    
+    if has_body == true
+#      while line = @request.gets
+#        puts "#{[line]}"
+#      end
+#      puts "#{[line]}"
+    end
     
   end
 end
 
-class Response # generates generic OK response to send to the client
+# generates generic OK response to send to the client
+class Response
   attr_reader :version, :response_code, :response_phrase, :headers, :body
   
   def initialize
@@ -112,7 +130,7 @@ class HttpdConf
       @httpdhash[:docRoot] = find("DocumentRoot")
     end
     def port
-      @httpdhash[:port] = find("port").to_i
+      @httpdhash[:port] = find("Listen").to_i
     end
     def log
       @httpdhash[:log] = find("LogFile")
