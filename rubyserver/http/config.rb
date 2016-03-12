7336385d04dc4dbce3321d4f94de2d3022d91dfd
devinclary <devinclary@me.com>
2016-03-12 15:10:28 -0800

    Changes to support better htaccess handling: -Class HttpConfig now returns .htaccess for access file name by default if none exists in the config file -Class HtaccessChecker only appends the access file name onto that path parameter if the parameter doesn't already end with the access file name (for security) -Class HtaccessChecker checks the path parameter for a valid access file. If the file exists in the path, the protected? method returns true -Class HtaccessChecker returns true for method authorized? if protected? returns false. -Class HtaccessChecker now opens the specified access file in the directory passed in as a parameter to check for access to any directory. -Removes empty () from method names in config.rb

--- rubyserver/http/config.rb
+++ rubyserver/http/config.rb
@@ -8,8 +8,8 @@ class ConfigFile
 
   # Return hash of key value pairs found in the multi-line string,
   # The key and value are separated by the first whitespace found in each line 
-  def load()
-    parsedData = Hash.new()
+  def load
+    parsedData = Hash.new
     @lines.each_line do |line|
       if self.valid_line?(line)
         lineSplit=line.split(' ', 2) #split into max two parts
@@ -42,7 +42,7 @@ class MimeTypes < ConfigFile
   # Take the values from @lines as @mime_type keys
   # Split the @lines values into multiple extensions delimited by white space
   def process_lines(line_hash)
-    @mime_types=Hash.new()
+    @mime_types=Hash.new
     line_hash.each do |mime_type, mime_extensions|
       mime_extensions.split(' ').each do |mime_extension|
         @mime_types[mime_extension]=mime_type
@@ -50,8 +50,8 @@ class MimeTypes < ConfigFile
     end
   end
 
-  # Override load() on superclass and add call to process_lines
-  def load()
+  # Override load on superclass and add call to process_lines
+  def load
     line_hash=super
     self.process_lines(line_hash)
     return self
@@ -68,53 +68,53 @@ class HttpConfig < ConfigFile
 
   def initialize(str)
     super
-    self.process_lines()
+    self.process_lines
   end
 
-  def process_lines()
-    @config=Hash.new("")
-    line_hash=load() #retrieves hash of the multi-line string
-    @config['ScriptAlias']=Hash.new() # Multiple ScriptAliases/Aliases may exist
-    @config['Alias']=Hash.new()
-    @config['DirectoryIndex']=Array.new() # Multiple DirectoryIndexes may exist
+  def process_lines
+    @config=Hash.new()
+    line_hash=load #retrieves hash of the multi-line string
+    @config['ScriptAlias']=Hash.new # Multiple ScriptAliases/Aliases may exist
+    @config['Alias']=Hash.new
+    @config['DirectoryIndex']=Array.new # Multiple DirectoryIndexes may exist
     line_hash.each do |config_option, config_values|
       if(config_option=='ScriptAlias') #Store all values in nested Hash
         config_values=config_values.split(' ')
-        @config['ScriptAlias'][config_values[0]]=config_values[1].strip()
+        @config['ScriptAlias'][config_values[0]]=config_values[1].strip
       elsif(config_option=='Alias') #Store all Alias values in one nested hash
         config_values=config_values.split(' ')
-        @config['Alias'][config_values[0]]=config_values[1].strip()
+        @config['Alias'][config_values[0]]=config_values[1].strip
       elsif(config_option=='DirectoryIndex')
         config_values.split(' ').each_with_index do |value, iteration|
-          @config['DirectoryIndex'][iteration]=value.strip()
+          @config['DirectoryIndex'][iteration]=value.strip
         end
       else
-        @config[config_option]=config_values.strip()
+        @config[config_option]=config_values.strip
       end
     end
   end
 
-  def server_root()
+  def server_root
     return @config['ServerRoot']
   end
 
-  def document_root()
+  def document_root
     return @config['DocumentRoot']
   end
 
-  def listen()
+  def listen
     return @config['Listen']
   end
 
-  def log_file()
+  def log_file
     return @config['LogFile']
   end
 
-  def alias_sym()
+  def alias_sym
     return @config['Alias']
   end
 
-  def alias_abs()
+  def alias_abs
     return @config['Alias']
   end
 
@@ -126,24 +126,27 @@ class HttpConfig < ConfigFile
     return @config['ScriptAlias'][path]
   end
 
-  def access_file_name()
+  def access_file_name
+    if not @config['AccessFileName']
+      return '.htaccess'
+    end
     return @config['AccessFileName']
   end
 
-  def directory_indexes()
+  def directory_indexes
     return @config['DirectoryIndex']
   end
 end
 
 # Here's some code to illustrate how this works!
-# mimes=MimeTypes.new(File.open("config/mime.types", "r").read()).load
+# mimes=MimeTypes.new(File.open("config/mime.types", "r").read).load
 # mimes.mime_types.each do |k,v|
 #   print k, " : ",v, "\n"
 # end
 # puts mimes.for('h261')
 
-# httpd_conf=HttpConfig.new(File.open("../config/httpd.conf", "r").read())
-# print "Server Root: ", httpd_conf.server_root(),"\n"
-# print "Document Root: ", httpd_conf.document_root(),"\n"
-# print "Listen: ", httpd_conf.listen(),"\n"
-# print "Log File: ",httpd_conf.log_file(),"\n"
+# httpd_conf=HttpConfig.new(File.open("../config/httpd.conf", "r").read)
+# print "Server Root: ", httpd_conf.server_root,"\n"
+# print "Document Root: ", httpd_conf.document_root,"\n"
+# print "Listen: ", httpd_conf.listen,"\n"
+# print "Log File: ",httpd_conf.log_file,"\n"
--- rubyserver/http/htaccessChecker.rb
+++ rubyserver/http/htaccessChecker.rb
@@ -1,18 +1,26 @@
 require_relative 'htaccess'
+require_relative 'config'
 require 'base64'
 require 'digest'
-
+require 'pathname'
 class HtaccessChecker
   attr_reader :path, :headers
   
-  def initialize(path, headers)
-    @path = path
+  # 'path' parameter can be both a path object or a path string
+  def initialize(path, headers, config) 
     @headers = headers
+    @config = config
+    #For security, check if the path parameter already points to an access file
+    if path.to_s.end_with? '/'+@config.access_file_name
+      @path=Pathname.new(path.to_s)
+    else
+      @path=Pathname.new(path.join(@config.access_file_name))
+    end
+    puts @path
   end
   
   def protected?
-    #@filepath.include?(Dir.pwd) ? true : false
-    if @path.include? "/home/izaacg/3_5webserver/SFSU_CSC_667/rubyserver/public_html/protected/" 
+    if File.exist?(@path)
       return true
     else 
       return false
@@ -29,15 +37,25 @@ class HtaccessChecker
   end
 
   def authorized?
+
+    if not self.protected?
+      return true
+    end
+
+    if not self.can_authorize?
+      return false
+    end
+
     user_identif = headers['Authorization']
     pass = user_identif.split(" ")
     decoded_ident = Base64.decode64(pass[1])
-    htaccess = Htaccess.new(File.open("/home/izaacg/3_5webserver/SFSU_CSC_667/rubyserver/public_html/protected/.htaccess", "r").read())
-    @auth_user_file = htaccess.auth_user_file
-    htpwd_file = File.open(@auth_user_file, "r")
+    htaccess = Htaccess.new(File.open(@path, "r").read())
+    auth_user_file = htaccess.auth_user_file
+    htpwd_file = File.open(auth_user_file, "r")
     htpwd_content = htpwd_file.read()
     username, password = decoded_ident.split(':')
     htpwd_array = htpwd_content.split("\n")
+
     htpwd_array.each do |content|
       htpasswd_parts = content.split(':')
       compare_string = htpasswd_parts[1].gsub(/{SHA}/, '')
